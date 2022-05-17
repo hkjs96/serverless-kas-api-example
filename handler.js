26fbbb7ba6eb2857ea2c5b36ec37a28420c1de8a
hkjs96 <hkjs96@naver.com>
2022-05-17 19:03:44 +0900

    kas, caver로 수정중

--- handler.js
+++ handler.js
@@ -1,11 +1,36 @@
-
+const { CaverExtKAS } = require("caver-js-ext-kas");
 const { SQS } = require("aws-sdk");
 
+// https://www.daleseo.com/js-dotenv/ 참고
+require("dotenv").config({ path: "./.env" });
+const chainId = process.env.CHAIN_ID;
+const accessKeyId = process.env.ACCESS_KEY;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
 const sqs = new SQS();
+
+// Set an authorization through constructor
+const caver = new CaverExtKAS(chainId, accessKeyId, secretAccessKey);
+
+/*
+  추가하기!
+  The example below introduces how to use caver.kct.kip7.
+*/
 
 const producer = async (event) => {
   let statusCode = 200;
   let message;
+
+  /*
+    요청 Body에 해당 내용이 오나 확인해 볼것
+    {
+      "contractAddress": "0x32dbcf48cdaa93673a8c642423cb5df4fdf4f469",
+      "fromAddress": "0x2FCf7A2bBb08088CcF2e2080c611D3BEC910EbB6",
+      "toAddress": "0x2FCf7A2bBb08088CcF2e2080c611D3BEC910EbB6",
+      "amount": 1000000000000000000,
+      "webhookURL": "https://yourdomain.com/webhook"
+    }
+  */
 
   if (!event.body) {
     return {
